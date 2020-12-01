08c55ca9d9c7e804510f4911426f069f51a4d14a
joe walsh <joewalsh@vistulaofindiana.com>
2020-12-01 02:22:10 -0500

    first

---
+++ goat_win/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\joewa\google drive\pythonwork\projects\goat\goat_win\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
