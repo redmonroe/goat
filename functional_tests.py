d36cb6f2de1c67baa5181cf7f3d194dd62856e11
joe walsh <joewalsh@vistulaofindiana.com>
2020-12-02 02:26:14 -0500

    second commit

--- functional_tests.py
+++ functional_tests.py
@@ -1,13 +1,27 @@
 # functional_tests.py
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
+import unittest
 
+class NewVisitorTest(unittest.TestCase):  
 
-# options = webdriver.ChromeOptions()
-# browser = webdriver.Chrome(executable_path=r"C:\Windows\chromedriver_win32\chromedriver.exe", options=options)
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+    def setUp(self):  
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):  
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)  
+        self.fail('Finish the test!')  
+
+        # She is invited to enter a to-do item straight away
+        # [...rest of comments as before]
+
+if __name__ == '__main__':  
+    unittest.main()
