b10031e6fbee434f6e00c2dd5b14c69e36ba6e0f
joe walsh <joewalsh@vistulaofindiana.com>
2020-12-03 22:32:12 -0500

    added first model text

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,38 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
+from lists.models import Item
 from lists.views import home_page  
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')  
-        self.assertEqual(found.func, home_page)  
+    def test_uses_home_template(self):
+        #client.get = GET method
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        print(html)
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('<html>'))
+    def test_can_save_a_POST_request(self):
+        # self.client.post: POST method, data is the data we are sending
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
--- lists/views.py
+++ lists/views.py
@@ -7,4 +7,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title><html>')
+     return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''), # .get supplies a default value of empty string
+    })
+    # return HttpResponse('<html><title>To-Do lists</title><html>')
